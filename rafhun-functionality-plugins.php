b0c0cae33b5f43b5ddaebacf98d34c656774c80a
Raphael Hüni <rafhun@gmail.com>
2015-12-04 07:10:13 +0100

    docs: add some documentation

--- rafhun-functionality-plugins.php
+++ rafhun-functionality-plugins.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Functionality Plugins
  * Plugin URI: https://github.com/rafhun/rafhun-functionality-plugins
- * Description: A collection of usefule and typical WP optimizations  brought together in one plugin. Comment out everything you do no need to keep your code base clean. Add more partials where needed. Always make sure to keep theme related stuff in the functions.php file of your theme while whole page related stuff should be here. For now there are two plugins in this collection, an editor optimization and a category navigation one. More will follow.
+ * Description: A collection of useful and typical WP optimizations  brought together in one plugin. Comment out everything you do no need to keep your code base clean. Add more partials where needed. Always make sure to keep theme related stuff in the functions.php file of your theme while whole page related stuff should be here. For now there are two plugins in this collection, an editor optimization and a category navigation one. More will follow.
  * Version: 0.1.0
  * Author: rafhun
  * Author URI: https://github.com/rafhun
