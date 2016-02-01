17f34104095625e41fad31550ff1b39b6ac892df
Raphael Hüni <hueni@werbelinie.ch>
2016-02-01 07:56:06 +0100

    fix(i18n): correctly include languages and rename the files

--- rafhun-functionality.php
+++ rafhun-functionality.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Functionality Plugins
- * Plugin URI: https://github.com/rafhun/rafhun-functionality-plugins
+ * Plugin URI: https://github.com/rafhun/rafhun-functionality
  * Description: A collection of useful and typical WP optimizations  brought together in one plugin. Comment out everything you do no need to keep your code base clean. Add more partials where needed. Always make sure to keep theme related stuff in the functions.php file of your theme while whole page related stuff should be here. For now there are two plugins in this collection, an editor optimization and a category navigation one. More will follow.
  * Version: 0.1.0
  * Author: rafhun
@@ -16,3 +16,9 @@ require 'rafhun-functionality-adjacent-category.php';
 require 'rafhun-functionality-custom-post-types.php';
 require 'rafhun-functionality-editor.php';
 require 'rafhun-functionality-head-cleanup.php';
+
+function rafhun_load_textdomain() {
+  load_plugin_textdomain( 'rafhun-functionality', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+}
+
+add_action( 'plugins_loaded', 'rafhun_load_textdomain' );
