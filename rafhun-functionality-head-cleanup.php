d0ffff66086efdf92c32bb22f33174398f652a49
Raphael Hüni <hueni@werbelinie.ch>
2016-02-01 07:55:35 +0100

    fix(head cleanup): no comment script removal is necessary
    The script is included if the comments are included in a sidebar. Make sure to remove the
    widget from it if you do not have a sidebar in your theme and if you do not need comments.

--- rafhun-functionality-head-cleanup.php
+++ rafhun-functionality-head-cleanup.php
@@ -16,11 +16,4 @@ remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
-
-// remove comments reply script
-function clean_header(){
-  wp_deregister_script( 'comment-reply' );
-}
-
-add_action('init','clean_header');
 ?>
